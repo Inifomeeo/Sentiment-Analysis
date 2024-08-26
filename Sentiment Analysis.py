8810cac874f9f7640b0a15d4dde2bec1d719b293
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-08-26 18:59:15 +0100

    Install NLTK data

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
